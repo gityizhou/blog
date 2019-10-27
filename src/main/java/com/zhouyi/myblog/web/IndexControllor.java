630283cd06f1350f443b75a0679b0e026d2721be
gityizhou <zxc47POI>
2019-10-27 15:54:07 +0800

    日志处理

--- src/main/java/com/zhouyi/myblog/web/IndexControllor.java
+++ src/main/java/com/zhouyi/myblog/web/IndexControllor.java
@@ -4,17 +4,18 @@ package com.zhouyi.myblog.web;
 import com.zhouyi.myblog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexControllor {
 
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name){
 //        int i = 1/0;
-        String blog = null;
-        if(blog == null){
-            throw new NotFoundException("博客不存在");
-        }
+//        String blog = null;
+//        if(blog == null){
+//            throw new NotFoundException("博客不存在");
+//        }
         return "index";
     }
 }
